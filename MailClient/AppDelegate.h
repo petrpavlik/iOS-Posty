168df9d2282b3f55ac862497ccaef29c230a6af0
Petr Pavlik <pavlipe7@gmail.com>
2014-07-19 11:03:52 +0200

    Initial Commit

---
+++ MailClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MailClient
+//
+//  Created by Petr Pavlik on 19/07/14.
+//  Copyright (c) 2014 Petr Pavlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MailClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MailClient
+//
+//  Created by Petr Pavlik on 19/07/14.
+//  Copyright (c) 2014 Petr Pavlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
