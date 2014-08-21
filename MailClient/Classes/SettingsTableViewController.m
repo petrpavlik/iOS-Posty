9161a37bd8fe16dbe4fbe84e09e9ee67a5c5ea33
Petr Pavlik <pavlipe7@gmail.com>
2014-08-21 21:38:28 +0200

    list linked accounts in settings

--- MailClient/Classes/SettingsTableViewController.m
+++ MailClient/Classes/SettingsTableViewController.m
@@ -22,9 +22,16 @@
     
     self.title = @"Settings";
     
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemEdit target:self action:@selector(editSelected)];
+    
     [self.tableView registerClass:[TitleValueTableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
     
+    SkinProvider* skin = [SkinProvider sharedInstance];
+    self.tableView.backgroundColor = skin.cellBackgroundColor;
+    self.tableView.separatorColor = skin.cellSeparatorColor;
+    
     UIButton* signOutButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    signOutButton.tintColor = [UIColor redColor];
     signOutButton.frame = CGRectMake(0, 0, 320, 44);
     [signOutButton setTitle:@"Sign Out" forState:UIControlStateNormal];
     
@@ -36,35 +43,72 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    
+    if (section==0) {
+        return 1;
+    }
+    else {
+        return 1;
+    }
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
     
-    // Configure the cell...
-    cell.textLabel.text = @"Signature";
+    SkinProvider* skin = [SkinProvider sharedInstance];
     
-    NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
-    NSString* htmlSignature = [userDefaults stringForKey:MailSignatureKey];
+    cell.textLabel.textColor = skin.headerTextColor;
+    cell.detailTextLabel.textColor = skin.textColor;
     
-    NSAttributedString* signarute = [[NSAttributedString alloc] initWithData:[htmlSignature dataUsingEncoding:NSUTF8StringEncoding]
-                                                                     options:@{NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
-                                                                               NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)}
-                                                          documentAttributes:nil error:nil];
-    
-    NSString* plaintextSignature = [signarute.string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    
-    cell.detailTextLabel.text = plaintextSignature;
-    
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    if (indexPath.section == 0) {
+        
+        // Configure the cell...
+        cell.textLabel.text = [INAPIManager shared].namespaceEmailAddresses[0];
+        
+        cell.detailTextLabel.text = nil;
+        
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+
+    }
+    else {
+        
+        // Configure the cell...
+        cell.textLabel.text = @"Signature";
+        
+        NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
+        NSString* htmlSignature = [userDefaults stringForKey:MailSignatureKey];
+        
+        NSAttributedString* signarute = [[NSAttributedString alloc] initWithData:[htmlSignature dataUsingEncoding:NSUTF8StringEncoding]
+                                                                         options:@{NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+                                                                                   NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)}
+                                                              documentAttributes:nil error:nil];
+        
+        NSString* plaintextSignature = [signarute.string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        
+        cell.detailTextLabel.text = plaintextSignature;
+        
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+
+    }
     
     return cell;
+}
+
+- (NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    
+    if (section==0) {
+        return @"Inboxes";
+    }
+    else {
+        return @"Settings";
+    }
+    
+    return nil;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -89,6 +133,12 @@
     [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
     
     [self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark -
+
+- (void)editSelected {
+    
 }
 
 @end
